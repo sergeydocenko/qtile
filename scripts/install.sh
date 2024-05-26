ddf0452be411b35ea6f0fbb0a78d657872bc8969
Sergii Dotsenko <sergey.docenko@gmail.com>
2024-05-26 15:08:11 +0300

    feat (tmux): Rebinding feat: Installer scrip update feat (sxhkd): Rebindinding

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo pacman -S volumeicon libnotify dunst nm-applet redshift picom
+sudo pacman -S volumeicon libnotify dunst nm-applet redshift picom tmux tmuxp
 yay -S greenclip
